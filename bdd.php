00e108a703c05c4c2b8ff7b3f1354abd5794b735
chouaibou <choumarega@gmail.com>
2024-06-17 16:16:04 +0200

    wordpress

---
+++ bdd.php
@@ -0,0 +1,6 @@
+<?php
+    require_once $_SERVER ['DOCUMENT_ROOT'] . '/wordpresstest/wp-load.php';
+    echo "ok";
+?>
+    
+
---
+++ wp-content/themes/montheme/functions.php
@@ -0,0 +1,6 @@
+<?php
+add_action('wp_head', function() {
+    die('salut les gens');
+});
+
+add_theme_support('title-tag');
