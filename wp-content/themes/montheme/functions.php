ea8e96ce62ea17a8f66496c84e09e4df63798637
chouaibou <choumarega@gmail.com>
2024-06-18 00:06:57 +0200

    actions et filtres wordpress

--- wp-content/themes/montheme/functions.php
+++ wp-content/themes/montheme/functions.php
@@ -1,6 +1,21 @@
 <?php
-add_action('wp_head', function() {
-    die('salut les gens');
-});
+
+function montheme_supports (){
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+}
+
+function montheme_register_assets_css (){
+    wp_enqueue_style('style', get_template_directory_uri().'/style.css');
+}
+
+function montheme_register_assets (){
+    wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' );
+    
+
+    wp_enqueue_style('bootstrap');
+    wp_enqueue_script('bootstrap');
+}
+
 
 add_theme_support('title-tag');
--- wp-content/themes/montheme/index.php
+++ wp-content/themes/montheme/index.php
@@ -1,4 +1,32 @@
 <?php get_header() ?>
-Bonjour tout le monde
+
+<?php  if (have_posts()) :?>
+    
+        <?php while (have_posts()) : the_post();?>
+           
+        <div class="card" style="width: 18rem;">
+       
+        <div class="card-body">
+            <?php the_post_thumbnail() ?>
+            <img src="" alt="">
+            <h5 class="titre"><?php  the_title();?></h5>
+
+            <p class="auteur"><?php the_author();?></p>
+           
+            <p class="contenu"><?php the_content();?></p>
+            
+
+
+            <a href="<?php the_permalink() ?>" class="btn btn-primary">Lire plus</a>
+        </div>
+        </div>
+
+        <?php endwhile ?>
+    
+<?php else: ?>
+    <p>Sorry, no posts matched your criteria.</p>
+
+<?php endif; ?>
+
 
 <?php get_footer() ?>
