9493c6dd410b98e859e9e223c013521e85283140
chouaibou <choumarega@gmail.com>
2024-06-18 13:10:24 +0200

    Gestion des menus

--- wp-content/themes/montheme/functions.php
+++ wp-content/themes/montheme/functions.php
@@ -1,17 +1,23 @@
 <?php
 
-function montheme_supports (){
+function montheme_supports()
+{
     add_theme_support('title-tag');
     add_theme_support('post-thumbnails');
+    add_theme_support('menus');
+    register_nav_menu('header', 'En tete du menu');
+
 }
 
-function montheme_register_assets_css (){
-    wp_enqueue_style('style', get_template_directory_uri().'/style.css');
+function montheme_register_assets_css()
+{
+    wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
 }
 
-function montheme_register_assets (){
-    wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css' );
-    
+function montheme_register_assets()
+{
+    wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css');
+
 
     wp_enqueue_style('bootstrap');
     wp_enqueue_script('bootstrap');
